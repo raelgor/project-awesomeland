9da2b0dab5d66418f1ecb28580569292c973380e
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-06-08 01:41:19 +0300

    Added components
    Added IP-based security.
    Added compression.
    Added caching.

--- main.js
+++ main.js
@@ -3,11 +3,56 @@ var http = require('http'),
     bodyParser = require('body-parser'),
     server = express(),
     jade = require('jade'),
-    package = require('./package.json');
+    package = require('./package.json'),
+    bouncer = require('http-bouncer'),
+    compression = require('compression');
 
-server.use(bodyParser());
+// Add bouncing rules
+bouncer.config.JSON_API_CALLS.push({
+    MATCH: {
+        api: "core",
+        request: "login"
+    },
+    INTERVAL: 10000,
+    LIMIT: 10,
+    INCLUDE_IP: true,
+    INCLUDE_FROM_MATCH: ["username"]
+});
 
+// Limiting request size (general upload size)
+server.use(bodyParser.raw({
+    limit: '5m'
+}));
+
+// Bounce by IP if request is of valid size
+server.use('*', function (req, res, next) {
+    bouncer({
+        connection: {
+            remoteAddress: req.connection.remoteAddress
+        }
+    }, res, next);
+});
+
+// Parse json api requests
+server.use(bodyParser.json({
+    extended: true
+}));
+
+// Use compression on all requests
+server.use(compression());
+server.disable('x-powered-by');
+
+// Add server stamp globally
+server.use('*', function (req, res, next) {
+    res.setHeader('Server', 'ZenX/' + package.version);
+    return next();
+});
+
+// Get app
 server.get('/', function (req, res, next) {
+
+    // Cache
+    res.setHeader("Cache-Control", "max-age=31104000");
 
     var fn = jade.compileFile(__dirname + '/index.jade');
 
@@ -15,16 +60,25 @@ server.get('/', function (req, res, next) {
         version: package.version
     }));
 
-    // res.sendFile(__dirname + '/index.html');
-
 });
 
+// Handle API calls
 server.post('/api', function (req, res, next) {
     
     res.send('ti leei1' + req.body.value);
 
 });
 
+// Cache if proceeding to static content
+server.use(function (req, res, next) {
+
+    res.setHeader("Cache-Control", "max-age=31104000");
+    return next();
+
+});
+
+// Directly access static content
 server.use(express.static(__dirname + '/'));
 
+// Bind to port and start
 http.createServer(server).listen(81, '10.240.203.106');
