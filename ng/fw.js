b76c71cbfb5e78268ab8657383bad7704913ae47
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-06-08 18:34:59 +0300

    Declared angular app

--- ng/fw.js
+++ ng/fw.js
@@ -1,1 +1,12 @@
-var fw = {}
+var fw = {},
+    app = angular.module('app', []);
+
+app.controller('main', ["$http", function ($http) {
+
+    fw.send = function () {
+
+        $http.post('/api', {api:"core",request:"login",username:"raelgor"}).success( function (r) { console.log(r); })
+
+    };
+
+}]);
