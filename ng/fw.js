f61318dfcdfcd3aca47ce0481f24bc6c1de24f99
jiyu92 <mantaslef@hotmail.gr>
2015-06-10 22:56:34 +0300

    Created fw.js

--- ng/fw.js
+++ ng/fw.js
@@ -1,12 +1,17 @@
 var fw = {},
     app = angular.module('app', []);
 
+app.run(['$http', function ($http) {
+
+    fw.send = function (data, successCallback, errorCallback) {
+
+        $http.post('/api', data).success(successCallback).error(errorCallback);
+
+    }
+
+}]);
+
 app.controller('main', ["$http", function ($http) {
 
-    fw.send = function () {
-
-        $http.post('/api', {api:"core",request:"login",username:"raelgor"}).success( function (r) { console.log(r); })
-
-    };
 
 }]);
