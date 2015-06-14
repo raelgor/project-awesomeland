32b36dc3b0067df66abf9402163e0e9084d0f1fd
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-06-14 20:20:11 +0300

    Login UI

--- ng/fw.initUILogin.js
+++ ng/fw.initUILogin.js
@@ -4,8 +4,18 @@
 
     window.session_token = userData.token;
 
+    $("#side-box > *").toggleClass("out");
+
+    $("#player-name").html(userData.first_name + " " + userData.last_name);
+
+    var picturePath = "https://graph.facebook.com/" + userData.fbid + "/picture";
+    $("#fbpic").css("background-image","url(" + picturePath + ")");
+
+    $("#nickname").html(userData.nickname);
+
 
 
     console.log(userData);
 
 }
+
