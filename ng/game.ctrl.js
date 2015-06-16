97abaeeb9df63c748b00cad6b6bd101e11b4378a
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-06-16 23:36:58 +0300

    Audio queue

--- ng/game.ctrl.js
+++ ng/game.ctrl.js
@@ -1,13 +1,35 @@
 ﻿app.controller('game', ['$scope', function ($scope) {
 
-    setTimeout(function () {
-        $('.gui').removeClass('unborn');
-    }, 100);
+    var audio = [
+        '/audio/heaven-ish.ogg',
+        '/audio/ambient-1.ogg',
+        '/audio/mystery.ogg',
+        '/audio/zone_theme.ogg'
+    ], _audio = {};
 
-    var bgMusic = new Audio();
+    setTimeout(function () { $('.gui').removeClass('unborn'); }, 100);
 
-    bgMusic.src = '/audio/heaven-ish.ogg';
-    bgMusic.play();
+    audio.forEach(function (path) {
+
+        var key = path.split('/').pop().split('.')[0],
+            audio = new Audio();
+
+        audio.src = path;
+
+        audio.onended = function () {
+
+            var next = Object.keys(_audio)[Object.keys(_audio).indexOf(key) + 1];
+            _audio[next].play();
+
+        }
+
+        _audio[key] = audio;
+
+    });
+
+    audio = _audio;
+
+    audio[Object.keys(audio)[0]].play();
 
     fw.connectWebSocket();
 
