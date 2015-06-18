56357125fee8397fd954d23b4bf8f651b1b455cc
Tsakbam <gronzie@hotmail.com>
2015-06-18 21:08:17 +0300

    Created file > open window

--- ng/fw.me.dialog.newMap.js
+++ ng/fw.me.dialog.newMap.js
@@ -7,8 +7,13 @@
     element.addClass('new-map-wrapper');
 
     // Fill with HTML
+    element.append('<div class="map-name">New map name:</div>');
     element.append('<input id="new-map-name" type="text"/>');
-    element.append('<input id="create-new-map" type="button" value="Create"/>');
+    element.append('<div class="x">x:</div>');
+    element.append('<input id="new-map-x" type="text"/>');
+    element.append('<div class="y">  /  y:</div>');
+    element.append('<input id="new-map-y" type="text"/>');
+    element.append('<input id="create-new-map" type="button" value="Create Map"/>');
 
     fw.createWindow({
         title: "New map",
