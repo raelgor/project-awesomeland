f0e1e54c922bb6086f091743cb702b7458df3b6e
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-06-18 19:52:24 +0300

    newMap example

--- ng/fw.me.dialog.newMap.js
+++ ng/fw.me.dialog.newMap.js
@@ -1,8 +1,18 @@
 ﻿fw.me.dialog.newMap = function () {
 
+    // Create HTML tag
+    var element = $('<div>');
+
+    // Add classes etc.
+    element.addClass('new-map-wrapper');
+
+    // Fill with HTML
+    element.append('<input id="new-map-name" type="text"/>');
+    element.append('<input id="create-new-map" type="button" value="Create"/>');
+
     fw.createWindow({
         title: "New map",
-        content: "Hi"
+        content: element
     });
 
 }
