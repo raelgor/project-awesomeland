9081212f439453e5c2ccd7e784cf71024a52253a
jiyu92 <mantaslef@hotmail.gr>
2015-06-18 19:47:15 +0300

    ##changes

---
+++ ng/fw.me.dialog.openMap.js
@@ -0,0 +1,22 @@
+﻿fw.me.dialog.openMap = function () {
+
+    var windowContent = $('<div>');
+    windowContent.html("<a>Loading...</a>");
+
+    fw.createWindow({
+        content: windowContent
+    });
+
+    fw.send({ api: "me", request: "avail-maps" }, true).success(function (response) {
+
+        windowContent.find('a').remove();
+
+        response.files.forEach(function (file) {
+
+            windowContent.append("<div>" + file + "</div>");
+
+        });
+
+    });
+
+};
--- ng/me.ctrl.js
+++ ng/me.ctrl.js
@@ -6,30 +6,5 @@
 
     $scope.fw = fw;
 
-    $scope.openMap = function () {
-
-        var windowContent = $('<div>');
-        windowContent.html("<a>Loading...</a>");
-
-        fw.createWindow({
-            content: windowContent
-        });
-
-        fw.send({ api: "me", request: "avail-maps" }, true).success(function (response) {
-
-            windowContent.find('a').remove();
-
-            response.files.forEach(function (file) {
-
-                windowContent.append("<div>" + file + "</div>");
-
-            });
-
-        });
-
-    }
-
-
-
 }]);
 
